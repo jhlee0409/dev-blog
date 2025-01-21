337d1fc1583558b3e1f77e5f0d00e6a02e9e6e44
jhlee0409 <relee6203@gmail.com>
2025-01-22 03:08:10 +0900

    💄 Badge 컴포넌트 css 수정

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -6,7 +6,7 @@ interface BadgeProps {
 
 export function Badge({ children }: BadgeProps) {
   return (
-    <span className="inline-flex items-center rounded-md bg-red-50 dark:bg-red-900/20 px-2 py-1 text-xs font-medium text-red-700 dark:text-red-400 ring-1 ring-inset ring-red-700/10 dark:ring-red-400/30">
+    <span className="inline-flex items-center rounded-md bg-red-50 dark:bg-red-900/20 px-2 py-0.5 text-sm font-medium text-red-700 dark:text-red-400 ring-1 ring-inset ring-red-700/10 dark:ring-red-400/30">
       {children}
     </span>
   );
