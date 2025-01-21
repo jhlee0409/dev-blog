ba51699b45777f550727d9aedbd32eeeebf4c253
jhlee0409 <relee6203@gmail.com>
2025-01-22 03:25:43 +0900

    ➕ rss 추가

---
+++ app/feed.xml/route.ts
@@ -0,0 +1,31 @@
+import { getBlogPosts } from "../blog/utils";
+import RSS from "rss";
+
+export async function GET() {
+  const feed = new RSS({
+    title: "DevUnpacker",
+    description: "Frontend Developer Blog",
+    site_url: "https://www.devunpacker.com",
+    feed_url: "https://www.devunpacker.com/feed.xml",
+    language: "ko",
+    pubDate: new Date(),
+  });
+
+  const allBlogs = getBlogPosts();
+
+  allBlogs.forEach((post) => {
+    feed.item({
+      title: post.metadata.title,
+      url: `https://www.devunpacker.com/blog/${post.slug}`,
+      date: post.metadata.publishedAt,
+      description: post.metadata.summary,
+      author: "Jack Lee",
+    });
+  });
+
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
