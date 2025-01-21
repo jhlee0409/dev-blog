eff9b7858e8a4080f79de4a7ef0240b57c24600a
jhlee0409 <relee6203@gmail.com>
2025-01-22 03:47:46 +0900

    🔧 리다이렉션 추가

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,8 @@ const redirectMap: Record<string, string> = {
   "/회고/2024+Recap": "/blog/2024-recap",
   "/항해+플러스+3기/항해플러스의+두+번째+코스%2C+클린코드를+마치며+-+FSD+편":
     "/blog/hanghae-3rd-fsd",
+  "%ED%95%AD%ED%95%B4+%ED%94%8C%EB%9F%AC%EC%8A%A4+3%EA%B8%B0/%ED%95%AD%ED%95%B4%ED%94%8C%EB%9F%AC%EC%8A%A4%EC%9D%98+%EB%91%90+%EB%B2%88%EC%A7%B8+%EC%BD%94%EC%8A%A4%2C+%ED%81%B4%EB%A6%B0%EC%BD%94%EB%93%9C%EB%A5%BC+%EB%A7%88%EC%B9%98%EB%A9%B0+-+FSD+%ED%8E%B8":
+    "/blog/hanghae-3rd-fsd",
   // 추가적인 리디렉션 규칙들을 여기에 추가할 수 있습니다
 };
 
